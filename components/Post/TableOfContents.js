4480fd0a46b8be6eb78767a5f5175d30b325097c
memoli0 <70433403+memoli0@users.noreply.github.com>
2024-07-11 19:11:56 +0300

    sd

--- components/Post/TableOfContents.js
+++ components/Post/TableOfContents.js
@@ -35,7 +35,7 @@ export default function TableOfContents ({ blockMap, frontMatter, pageTitle }) {
   return (
     <div
       className='hidden xl:block xl:fixed ml-4 text-sm text-gray-500 dark:text-gray-400 whitespace'
-      style={{ maxHeight: '80vh', overflowY: 'auto' }}
+      style={{ height: 'calc(100vh - 10px)', overflowY: 'auto', padding: '10px' }} //Fixxing scroll bug
     >
       {pageTitle && (
         <Link
