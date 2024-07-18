32c9f7bc4a26f982f8dabc441a4367dc2d789c5c
memoli0 <70433403+memoli0@users.noreply.github.com>
2024-07-18 17:18:12 +0300

    s

--- components/Post/NotionRenderer.js
+++ components/Post/NotionRenderer.js
@@ -1,34 +1,39 @@
-import React from 'react'
 import BLOG from '@/blog.config'
 import PropTypes from 'prop-types'
 import dynamic from 'next/dynamic'
 import { useRouter } from 'next/router'
+
 import { NotionRenderer as Renderer } from 'react-notion-x'
-import Prism from 'prismjs'
-import 'prismjs/themes/prism-tomorrow.css' 
-
-// Custom Code component
-const CustomCode = ({ code, language }) => {
-  const [highlighted, setHighlighted] = React.useState('')
-
-  React.useEffect(() => {
-    if (Prism.languages[language]) {
-      setHighlighted(Prism.highlight(code, Prism.languages[language], language))
-    } else {
-      setHighlighted(code)
-    }
-  }, [code, language])
-
-  return (
-    <pre className={`language-${language}`}>
-      <code dangerouslySetInnerHTML={{ __html: highlighted }} />
-    </pre>
-  )
-}
 
 // Lazy-load some heavy components & override the renderers of some block types
 const components = {
-  Code: CustomCode,
+  // Code block
+  Code: dynamic(() => {
+    return import('react-notion-x/build/third-party/code').then(async module => {
+      // Additional prismjs syntax
+      await Promise.all([
+        import('prismjs/components/prism-bash'),
+        import('prismjs/components/prism-c'),
+        import('prismjs/components/prism-cpp'),
+        import('prismjs/components/prism-docker'),
+        import('prismjs/components/prism-js-templates'),
+        import('prismjs/components/prism-diff'),
+        import('prismjs/components/prism-git'),
+        import('prismjs/components/prism-go'),
+        import('prismjs/components/prism-graphql'),
+        import('prismjs/components/prism-makefile'),
+        import('prismjs/components/prism-markdown'),
+        import('prismjs/components/prism-python'),
+        import('prismjs/components/prism-rust'),
+        import('prismjs/components/prism-solidity'),
+        import('prismjs/components/prism-sql'),
+        import('prismjs/components/prism-swift'),
+        import('prismjs/components/prism-wasm'),
+        import('prismjs/components/prism-yaml')
+      ])
+      return module.Code
+    })
+  }),
   // Database block
   Collection: dynamic(() => {
     return import('react-notion-x/build/third-party/collection').then(module => module.Collection)
@@ -37,45 +42,6 @@ const components = {
   Equation: dynamic(() => {
     return import('react-notion-x/build/third-party/equation').then(module => module.Equation)
   })
-}
-
-// Load additional Prism languages
-const loadPrismLanguages = async () => {
-  await Promise.all([
-    import('prismjs/components/prism-bash'),
-    import('prismjs/components/prism-c'),
-    import('prismjs/components/prism-cpp'),
-    import('prismjs/components/prism-css'),
-    import('prismjs/components/prism-docker'),
-    import('prismjs/components/prism-diff'),
-    import('prismjs/components/prism-git'),
-    import('prismjs/components/prism-go'),
-    import('prismjs/components/prism-graphql'),
-    import('prismjs/components/prism-java'),
-    import('prismjs/components/prism-javascript'),
-    import('prismjs/components/prism-js-templates'),
-    import('prismjs/components/prism-jsx'),
-    import('prismjs/components/prism-kotlin'),
-    import('prismjs/components/prism-latex'),
-    import('prismjs/components/prism-less'),
-    import('prismjs/components/prism-makefile'),
-    import('prismjs/components/prism-markdown'),
-    import('prismjs/components/prism-matlab'),
-    import('prismjs/components/prism-php'),
-    import('prismjs/components/prism-python'),
-    import('prismjs/components/prism-r'),
-    import('prismjs/components/prism-regex'),
-    import('prismjs/components/prism-ruby'),
-    import('prismjs/components/prism-rust'),
-    import('prismjs/components/prism-sass'),
-    import('prismjs/components/prism-scala'),
-    import('prismjs/components/prism-solidity'),
-    import('prismjs/components/prism-sql'),
-    import('prismjs/components/prism-swift'),
-    import('prismjs/components/prism-typescript'),
-    import('prismjs/components/prism-wasm'),
-    import('prismjs/components/prism-yaml')
-  ])
 }
 
 /**
@@ -87,11 +53,6 @@ const loadPrismLanguages = async () => {
  */
 export default function NotionRenderer (props) {
   const { locale } = useRouter()
-
-  React.useEffect(() => {
-    loadPrismLanguages()
-  }, [])
-
   const mapPageUrl = (id) => {
     // console.log('mapPageUrl', BLOG.lang.split('-')[0])
     if (locale === BLOG.lang.split('-')[0]) {
@@ -100,7 +61,6 @@ export default function NotionRenderer (props) {
       return '/' + locale + '/s/' + id.replace(/-/g, '')
     }
   }
-
   return (
     <Renderer
       components={components}
