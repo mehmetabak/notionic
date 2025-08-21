1881bd9fbf5d72aeffd8c20aa346c23ea843ea0f
Mehmet Abak <70433403+mehmetabak@users.noreply.github.com>
2025-08-21 20:24:27 +0300

    d

--- blog.config.js
+++ blog.config.js
@@ -24,7 +24,7 @@ const BLOG = {
     friends: true
   },
   showWeChatPay: false,
-  previewImagesEnabled: false, // Enable preview images in Notion renderer
+  previewImagesEnabled: true, // Enable preview images in Notion renderer
   autoCollapsedNavBar: false, // The automatically collapsed navigation bar
   ogImageGenerateHost: 'og-zl.vercel.app', // The link to generate OG image, don't end with a slash
   defaultCover: '/cover.jpg',
